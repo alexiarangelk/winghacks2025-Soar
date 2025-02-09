83d0017fada5c5c31ea833b5426884a2ed75ff96
Alexia Rangel Krashenitsa <73617301+alexiarangelk@users.noreply.github.com>
2025-02-08 19:18:38 -0500

    small update

--- soar/src/App.jsx
+++ soar/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {auth} from './components/firebase';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import Login from './components/login';
-import Registration from './components/Registration';
 import Routing from './components/routing';
 import Registration from './components/Registration';
 
